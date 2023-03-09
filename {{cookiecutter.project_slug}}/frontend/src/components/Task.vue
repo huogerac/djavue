b9a2725d039633fed1cdc447578d9db592e91f59
Roger Camargo <huogerac@gmail.com>
2023-03-08 23:39:29 -0300

    Versão inicial para substituir o vue cli (template) por cookiecutter

---
+++ {{cookiecutter.project_slug}}/frontend/src/components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <div>#{% raw %}{{ task.id }}{% endraw %}</div>
+      <p class="ma-0 pa-0 text-h5 text--primary">
+        {% raw %}{{ task.description }}{% endraw %}
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TasksModel",
+  props: {
+    task: {
+      type: Object,
+      default: null,
+      roger: "usalinter",
+    },
+  },
+  data: () => ({}),
+}
+</script>
